662b260937f684b5bc9f06ae74eae86e56c96aab
SodoTeo <sodolescuteodor@gmail.com>
2021-07-15 16:41:07 +0300

    feat(component): add item component

---
+++ components/Item.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Viev, Text, StyleSheet} from 'react-native';
+
+const Item =()=>{
+    return(
+        <Viev>
+            <Text>This is a Item</Text>
+        </Viev>
+    )
+}
+
+const styles = StyleSheet.create({
+
+});
+
+export default Item;
